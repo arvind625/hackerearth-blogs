c06349d9f28ba34dfa914bcbcbf784fada4dbe55
Arvind Dhakar <arvindradhakar625@gmail.com>
2022-08-22 16:54:10 +0530

    Added download feature and created navtab for projects and about

--- src/Resume.js
+++
@@ -1,8 +0,0 @@
-
-import { Image } from 'react-bootstrap';
-import React from 'react';
-
-export const Resume = () => (
-    <Image src="https://i.ibb.co/9c9hJ9M/Arvind-Resume.jpg" alt="Arvind-Resume" fluid />
-);
- 
--- src/Routing.js
+++ src/Routing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Blogs } from './Blogs';
 import { NewBlog } from './NewBlog';
-import { Resume } from './Resume';
 import { ShowBlog } from './ShowBlog';
 
 export const Routes = ({ blogs, setBlogs }) => (
@@ -13,8 +12,7 @@ export const Routes = ({ blogs, setBlogs }) => (
             path={['/blog', '/new']}
             to={'/blog/new'}
         />
-        <Route key='MY_RESUME' path='/' exact component={Resume} />
-        <Route path='/blogs' exact component={() => <Blogs blogs={blogs} />} />
+        <Route path={['/', '/blogs']} exact component={() => <Blogs blogs={blogs} />} />
         <Route path='/blog/new' exact component={() => <NewBlog setBlogs={setBlogs} />} />
         <Route path="/blogs/:id" exact component={() => <ShowBlog />} />
     </Switch>
